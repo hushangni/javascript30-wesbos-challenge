33c7725b9d9176ff4587f79480afd0cb0e7085d1
hushangni <shangnihu@gmail.com>
2018-07-11 14:44:01 -0400

    Finished play button on Day 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,25 @@
+// Get our Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out functions
+function togglePlay() {
+	video.paused ? video.play() : video.pause();
+}
+
+function updateButton() {
+	console.log('Update the button');
+	const icon = this.paused ?  '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
+// Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
